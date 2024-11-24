8634145006745c0bfb785398c8f8ff8f2097262a
sumeetCodes <sumeet71906981@gmail.com>
2024-11-24 06:01:55 +0000

    nothing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+
 function App() {
   return (
     <div className="App">
-     <h1>FIRST REact project</h1>
+     <h1 className='text-danger'>FIRST REact proect</h1>
     </div>
   );
 }
 
 export default App;
+
+
