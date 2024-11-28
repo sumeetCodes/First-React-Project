d79515a3fd0fa66a4b893daead59258a4e6d64ad
sumeetCodes <sumeet71906981@gmail.com>
2024-11-28 17:58:10 +0000

    image added

--- src/components/Herosection/Herosection.jsx
+++ src/components/Herosection/Herosection.jsx
@@ -1,3 +1,5 @@
+
+
 const Herosection = () => {
     return(
         <>
@@ -8,6 +10,7 @@ const Herosection = () => {
             <div className="col-sm-12 col-lg-6 col-md-6 col-12">
 <h1 className="fw-bold w-50 fs-1">Showcase your app beautifully.</h1>
 <p className="text-secondary w-75">Launch your mobile app landing page faster with this free, open source theme from Start Bootstrap!</p>
+ 
             </div>
 
 {/* right side */}
